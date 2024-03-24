34554ed20b5d7fcc99ba0924049bac55bdf986f2
prashantsingh34 <113302801+prashantsingh34@users.noreply.github.com>
2024-03-25 00:25:44 +0530

    Demo run

--- server/routes/post.routes.js
+++ server/routes/post.routes.js
@@ -14,4 +14,6 @@ router.route('/posts').post(PostController.addPost);
 // Delete a post by cuid
 router.route('/posts/:cuid').delete(PostController.deletePost);
 
+
+console.log("Checking for the demo run");
 export default router;
